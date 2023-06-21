373bd76b526f39c2786567612208ae2e665c9061
malhumaidan <89436547+malhumaidan@users.noreply.github.com>
2023-06-21 13:21:06 +0300

    Create books.dart

---
+++ books.dart
@@ -0,0 +1,26 @@
+
+  List books = [
+  Book(
+    name: 'A Song of Ice and Fire',
+    author: 'George R.R. Martin',
+    imageUrl:
+        'https://upload.wikimedia.org/wikipedia/en/d/dc/A_Song_of_Ice_and_Fire_book_collection_box_set_cover.jpg',
+  ),
+  Book(
+    name: 'The Alchemist',
+    author: 'Paulo Coelho',
+    imageUrl:
+        'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1654371463i/18144590.jpg',
+  ),
+  Book(
+    name: 'Who Moved My Cheese',
+    author: 'Spencer Johnson',
+    imageUrl: 'https://m.media-amazon.com/images/I/81GBTzGb+2L.jpg',
+  ),
+  Book(
+    name: 'Sherlock Holmes',
+    author: 'Sir Arthur Conan Doyle',
+    imageUrl:
+        'https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781607102113/the-adventures-of-sherlock-holmes-and-other-stories-9781607102113_hr.jpg',
+  ),
+];
